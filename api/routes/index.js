1e0c02821628647a078d7ec9e05303271dd756fa
Luis Lechuga <luislechu55@gmail.com>
2022-07-26 19:20:01 -0500

    feat: add Product Model

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,2 +1,3 @@
 export { default as costumerRoutes } from './costumerRoutes.js';
 export { default as adminRoutes } from './adminRoutes.js';
+export { default as productRoutes } from './productRoutes.js';
