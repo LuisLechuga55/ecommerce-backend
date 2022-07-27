3d628ad3e51759ec6640aaebb54dd9de41592693
Luis Lechuga <luislechu55@gmail.com>
2022-07-26 19:18:20 -0500

    feat: add admin middleware

--- api/middlewares/index.js
+++ api/middlewares/index.js
@@ -1,5 +1,7 @@
-export { default as authValidator } from './authValidator.js';
 export { default as createCostumValidator } from './createCostumValidator.js';
 export { default as loginCostumValidator } from './loginCostumValidator.js';
 export { default as createAdminValidator } from './createAdminValidator.js';
 export { default as loginAdminValidator } from './loginAdminValidator.js';
+export { default as protectedAdmin } from './protectedAdmin.js';
+export { default as protectedCostum } from './protectedCostum.js';
+
