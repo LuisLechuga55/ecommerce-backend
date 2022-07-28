03b8d8abdf921b1896b95b0c62aaf2aadd2ea031
Luis Lechuga <luislechu55@gmail.com>
2022-07-28 18:56:33 -0500

    fix: port change

--- api/config/index.js
+++ api/config/index.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config({});
 
 export default {
+  server: {
+    port: process.env.PORT || 4000,
+  },
   database: {
     uri: process.env.DB_URI,
     name: process.env.DB_NAME,
